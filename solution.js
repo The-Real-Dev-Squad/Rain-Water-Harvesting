16e246ffe85faaab87140cf0551e82768f9926f3
Nikhil Bhandarkar <nikhil.bhandarkar@waycool.in>
2020-05-24 20:16:37 +0530

    add comments

--- solution.js
+++ solution.js
@@ -11,12 +11,12 @@ function min(a, b) {
 }
 
 function waterTrapped(arr) {
-  if (!arr.length){
+  if (!arr.length){ // blank array
     return 0
   }
 
-  let left = []
-  let right = []
+  let left = [] // to store the left max respective current index
+  let right = [] // to store the right max respective of current index
   let answer = 0
 
   left[0] = arr[0]
@@ -30,14 +30,13 @@ function waterTrapped(arr) {
   }
 
   for (let i = 0; i<arr.length; i++) {
-    answer += min(left[i], right[i]) - arr[i]
+    answer += min(left[i], right[i]) - arr[i] // minimum of both left and right - current element (this is the water stored at current column)
   }
   
   return answer
 }
 
 // Test Cases
-
 console.log(waterTrapped([])) // 0
 console.log(waterTrapped([4,3,5,9,6,8,7,2,3,5,9])) // 24
 console.log(waterTrapped([1,0,3])) // 1
