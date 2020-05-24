5dda0c543ba469ad8632279b68abd9f8a6356e55
Akashdeep <adeep8961@gmail.com>
2020-05-24 20:15:46 +0530

    updates

--- rainWaterHarvest.py
+++ rainWaterHarvest.py
@@ -5,9 +5,9 @@ def rainWaterHarvest(a):
 	left_max[0]=a[0]
 	right_max[n-1]=a[n-1]
 	for i in range(1,n):
-	    left_max[i]=max(left_max[i-1],a[i])
+	    left_max[i]=max(left_max[i-1],a[i-1])
 	for i in range(n-2,-1,-1):
-	    right_max[i]=max(right_max[i+1],a[i])
+	    right_max[i]=max(right_max[i+1],a[i+1])
 	ans=0
 	for i in range(1,n):
 	    ans+=min(left_max[i],right_max[i])
